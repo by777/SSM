e93ee50768cc90b767c82f3386500fe4031f4c2d
by777 <1373953675@qq.com>
2020-08-23 23:56:17 +0800

    p148有bug

---
+++ 12.SpringMVC_output/src/com/atguigu/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String hello() {
+		System.out.println("Hello!请求！");
+		return "success";
+	}
+}
